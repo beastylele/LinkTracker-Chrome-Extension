2b80eff9bc15f4339979153d87e467ba4b46e5e4
beastylele <102832142+beastylele@users.noreply.github.com>
2024-04-01 17:41:25 -0400

    update index.js

--- index.js
+++ index.js
@@ -3,6 +3,13 @@ const inputEl = document.getElementById("input-el")
 const inputBtn = document.getElementById("input-btn")
 const ulEl = document.getElementById("ul-el")
 
+let leadsfromLocalStorage = JSON.parse(localStorage.getItem("myLeads"))
+
+if (leadsfromLocalStorage) {
+    myLeads = leadsfromLocalStorage
+    renderLeads()
+}
+
 inputBtn.addEventListener("click", function() {
     myLeads.push(inputEl.value)
     inputEl.value = ""
